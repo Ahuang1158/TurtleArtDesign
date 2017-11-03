991cb87b93a0ab05a464a588c027928b8ebaca7a
Ahuang1158 <33285327+Ahuang1158@users.noreply.github.com>
2017-11-03 08:43:46 -0400

    Add files via upload

--- project 3.py
+++ project 3.py
@@ -8,9 +8,9 @@ bob=turtle.Turtle()
 bob.speed(0)
 turtle.tracer(0)
 
-for times in range(1000):
-    x = randint( -900, 900)
-    y = randint(-900, 900)
+for times in range(2000):
+    x = randint( -800, 900)
+    y = randint(-800, 900)
     bob.color(255,255,255)
     move(bob,x,y)
     bob.begin_fill()
@@ -29,18 +29,19 @@ for times in range(500):
     bob.left(10)
 
 for times in range(500):
+    bob.color(148,0,211)
+    bob.width(3)
+    bob.circle(3000000)
+    bob.left(8)
+
+for times in range(500):
     bob.width(1)
     bob.color(255,255,255)
     petal(bob,100)
     bob.left(5)
 
 bob.color(0,0,0)
-move(bob,-114,136)
+move(bob,-178,30)
 bob.begin_fill()
 bob.circle(180)
 bob.end_fill()
-
-
-
-
-
